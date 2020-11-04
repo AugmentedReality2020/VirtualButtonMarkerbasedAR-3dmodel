989103717b45a5d2049d231aaade2ac69bfc704b
Naveenasri02 <66512636+Naveenasri02@users.noreply.github.com>
2020-11-04 19:10:57 +0530

    first commit

---
+++ VirtualButtonAR/Assets/Scripte/VirtualButtonScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class VirtualButtonScript : MonoBehaviour ,IVirtualButtonEventHandler
+{
+    public GameObject cargo, cyclego;
+    // Start is called before the first frame update
+    void Start()
+    {
+        VirtualButtonBehaviour[] vrb = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < vrb.Length; i++)
+        {
+            vrb[i].RegisterEventHandler(this);
+        }
+        cargo.SetActive(false);
+        cyclego.SetActive(false);
+    }
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        if (vb.VirtualButtonName == "CarVB")
+        {
+            Debug.Log("Car Button Pressed");
+            cargo.SetActive(true);
+            cyclego.SetActive(false);
+        }
+        else if(vb.VirtualButtonName == "CycleVB")
+        {
+            Debug.Log("Cycle Button Pressed");
+            cargo.SetActive(false);
+            cyclego.SetActive(true);
+        }
+        else
+        {
+            throw new UnityException(vb.VirtualButtonName + "Virtual Button not Supported");
+        }
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Virtual Button Pressed");
+    }
+}
